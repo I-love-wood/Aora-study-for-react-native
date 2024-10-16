8a8df3266086a9c5eca6fca93661ad54c43cf4eb
lzh2024v <3198725383@qq.com>
2024-10-16 23:17:29 +0800

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,18 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
+// 从自定义路径导入自定义的标签栏图标组件
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  /**
+   * 
+   * screenOptions:配置每个标签页的选项 
+   * tabBarActiveTintColor：设置活动标签的颜色，基于检测到的colorScheme
+   * colorScheme ?? 'light': 这是一个逻辑运算符，表示如果 colorScheme 的值为 null 或 undefined，则使用 'light' 作为默认值。意思是如果没有检测到用户的颜色方案，则默认使用亮模式的颜色方案。
+  */
   return (
     <Tabs
       screenOptions={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,29 @@
+/**引入导航的主题和用于处理它们的提供程序 */
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+/**引入加载自定义字体的钩子 */
 import { useFonts } from 'expo-font';
+/**引入栈导航 */
 import { Stack } from 'expo-router';
+/**引入启动屏幕模块 */
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+/**引入 React Native 重动画库 */
 import 'react-native-reanimated';
-
+/**引入自定义钩子以检测颜色方案（暗模式或亮模式）。 */
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
+// 防止初始屏幕在加载完成之前自动隐藏
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  /**使用 useColorScheme 钩子来确定用户偏好暗模式还是亮模式 */
   const colorScheme = useColorScheme();
+  /**加载自定义字体并将状态赋值给 loaded */
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
+  /**使用 useEffect 在字体加载完成后隐藏启动屏幕 */
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -25,7 +33,11 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-
+  /** Stack 组件包含多个 Stack.Screen 子组件，每个子组件定义一个页面
+   * name：页面的名称，作为导航的标识。
+   * component：要渲染的 React 组件，也就是页面内容。
+   * options 用于自定义每个页面的标题、样式等,设置 headerShown 为 false，隐藏页面的头部。
+   */
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
